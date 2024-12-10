c1288f77a9fa19bcfff8bdc9b0a1cc04adf93074
robert-orri <robertorriarnason@gmail.com>
2024-12-10 07:57:44 +0000

    Bæta við layoutið

--- dr-cinema/app/_layout.tsx
+++ dr-cinema/app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen 
+        name="index" 
+        options={{
+          headerTitle: 'Dr.Cinema',
+          headerTitleAlign: 'center',
+          headerLeft: () => null,
+        }}
+      />
+    </Stack>
+  );
 }
